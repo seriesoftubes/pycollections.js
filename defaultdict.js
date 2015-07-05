1abcdbafa3c77f0add91b5a3a0e93e9f3407fdd4
Brian Weidenbaum <brianweiden@gmail.com>
2015-07-05 11:18:33 -0700

    use less verbose .apply method for subclass .get()

--- defaultdict.js
+++ defaultdict.js
@@ -12,7 +12,7 @@ DefaultDict.prototype = Object.create(Dict.prototype);
 DefaultDict.prototype.get = function(key /*, defaultValue */) {
   // If .get(k, v), use super method.
   if (arguments.length === 2) {
-    return this.constructor.prototype.get.call(this, key, arguments[1]);
+    return this.constructor.prototype.get.apply(this, arguments);
   }
   return this.contains(key) ? this.dict_[key] : this.set(key, this.default_());
 };
