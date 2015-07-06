bbc03eac737c84b3c0ffe247d452b4b699bdb9fe
Brian Weidenbaum <brianweiden@gmail.com>
2015-07-05 18:54:07 -0700

    add more tests

--- defaultdict-test.js
+++ defaultdict-test.js
@@ -6,4 +6,16 @@ describe('Initialized empty defaultdict', function() {
     expect(function() { new DefaultDict(Number); }).not.toThrow();
   });
 
+  it('Returns its default value when a key is missing', function() {
+    var defaultFn = String;
+    var defaultFnValue = defaultFn();
+    var dd = new DefaultDict(defaultFn);
+
+    var missingKeys = [1, 2, 3];
+    missingKeys.forEach(function(key) {
+      expect(dd.contains(key)).toBe(false);
+      expect(dd.get(key)).toBe(defaultFnValue);
+    });
+  });
+
 });
