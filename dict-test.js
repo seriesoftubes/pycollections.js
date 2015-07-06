5c1bf4a110b1bdb5cafbf6308d2b6f20ad53f324
Brian Weidenbaum <brianweiden@gmail.com>
2015-07-05 17:55:43 -0700

    add tests for object-based initialization

--- dict-test.js
+++ dict-test.js
@@ -243,3 +243,100 @@ describe('Dict after initialization with non-empty non-unique array as the arg',
     expect(dict.get('b', defaultValue)).toBe(100);
   });
 });
+
+
+describe('Dict after initialization with empty object as the arg', function() {
+  var dict;
+
+  beforeEach(function() {
+    dict = new Dict({});
+  });
+
+  it('Has length of 0', function() {
+    expect(dict.length()).toBe(0);
+  });
+
+  it('Has no items', function() {
+    expect(dict.items()).toEqual([]);
+  });
+
+  it('Has no keys', function() {
+    expect(dict.keys()).toEqual([]);
+  });
+
+  it('Has no values', function() {
+    expect(dict.values()).toEqual([]);
+  });
+});
+
+
+describe('Dict after initialization with non-empty object as the arg', function() {
+  var dict;
+
+  var constructorArgs = {
+    'a': true,
+    'b': [1, 2, 3]
+  };
+
+  beforeEach(function() {
+    dict = new Dict(constructorArgs);
+  });
+
+  it('Has length of 2', function() {
+    expect(dict.length()).toBe(2);
+  });
+
+  it('Has some items', function() {
+    var items = dict.items();
+    expect(items).toContain(['a', true]);
+    expect(items).toContain(['b', [1, 2, 3]]);
+  });
+
+  it('Has some keys', function() {
+    var keys = dict.keys();
+    expect(keys).toContain('a');
+    expect(keys).toContain('b');
+    expect(keys).not.toContain('basd');
+  });
+
+  it('Has some values', function() {
+    var values = dict.values();
+    expect(values).toContain(true);
+    expect(values).toContain([1, 2 ,3]);
+  });
+
+  // these don't contain 'a' or 'b'
+  var possibleKeys = [
+      0, 1,
+      '', 'z',
+      false, true,
+      {}, {1: 'A'}
+  ];
+
+  it('contains only a and b', function() {
+    possibleKeys.forEach(function(key) {
+      expect(dict.contains(key)).toBe(false);
+    });
+    expect(dict.contains('a')).toBe(true);
+    expect(dict.contains('b')).toBe(true);
+  });
+
+  it('Returns undefined from get() with non-present key and no defaultValue', function() {
+    possibleKeys.forEach(function(key) {
+      expect(dict.get(key)).toBe(undefined);
+    });
+  });
+
+  it('Returns defaultValue from get() with any non-present key and a defaultValue', function() {
+    var defaultValue = {1: 2};
+    possibleKeys.forEach(function(key) {
+      expect(dict.get(key, defaultValue)).toBe(defaultValue);
+    });
+  });
+
+  it('Returns existing value from get() with existing key and a defaultValue', function() {
+    var defaultValue = {1: 2};
+    expect(dict.get('a', defaultValue)).toBe(true);
+    expect(dict.get('b', defaultValue)).toEqual([1, 2, 3]);
+  });
+});
