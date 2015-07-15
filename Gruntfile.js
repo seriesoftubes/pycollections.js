41ca3b244a428e861b5a38d2876fee7980d8f949
Brian Weidenbaum <brianweiden@gmail.com>
2015-07-14 23:43:51 -0700

    remove pointless newline from min.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
 
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */',
         preserveComments: 'some',
         mangle: true,
         compress: true,
