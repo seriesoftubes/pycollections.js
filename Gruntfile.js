86b8af7eb4a1fb722fdf5eb0c13e9cb723fd83a2
Brian Weidenbaum <brianweiden@gmail.com>
2015-07-23 06:52:34 -0700

    reorder namedtuple and ordereddict files

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,9 +24,8 @@ module.exports = function (grunt) {
           'src/2.dict.js',
           'src/3.defaultdict.js',
           'src/4.counter.js',
-          // TODO: make ordereddict 5 and namedtuple 6
-          'src/6.ordereddict.js',
-          'src/5.namedtuple.js',
+          'src/5.ordereddict.js',
+          'src/6.namedtuple.js',
           'src/footer'
         ],
         dest: CONCAT_FILE
